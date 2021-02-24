35d365defcfdafc3388f5c297fb24238e73f46f3
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-02-24 10:29:39 -0800

    Working on combining desk & deskquote into 1 class

--- MegaDesk/Models/DeskQuote.cs
+++ MegaDesk/Models/DeskQuote.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MegaDesk.Models
 {
+    public enum DesktopMaterials
+    {
+        Oak = 200,
+        Laminate = 100,
+        Pine = 50,
+        Rosewood = 300,
+        Veneer = 125
+    }
     public class DeskQuote
     {
-        
+        public int ID { get; set; } //Primary id
+        public string CustomerName { get; set; }
+        public DateTime QuoteDate { get; set; }
+        public int RushDays { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int Drawers { get; set; }
+        public DesktopMaterials SurfaceMaterial { get; set; }
     }
 }
