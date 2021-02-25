007109b6ad30dfaa7397a6247af8a1f1f4f54b5a
craigkehl <keh19003@byui.edu>
2021-02-25 13:50:12 -0700

    added sort by customer name and date added keyword  search by customer name added  Desk Quote Nav link Changed page title  and  h1 to Quote List

--- MegaDesk/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDesk/Pages/DeskQuotes/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDesk.Data;
 using MegaDesk.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging.EventSource;
 
 namespace MegaDesk.Pages.DeskQuotes
 {
@@ -20,10 +22,44 @@ namespace MegaDesk.Pages.DeskQuotes
         }
 
         public IList<DeskQuote> DeskQuote { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList SortOrderList { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string sortOrder { get; set; }
 
         public async Task OnGetAsync()
         {
-            DeskQuote = await _context.DeskQuote.ToListAsync();
+            IQueryable<string> orderQuery = from o in _context.DeskQuote
+                orderby o.CustomerName
+                select o.CustomerName;
+
+            var quotes = from q in _context.DeskQuote
+                select q;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                quotes = quotes.Where(keyword => keyword.CustomerName.Contains(SearchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "1":
+                    quotes = quotes.OrderBy(s => s.CustomerName);
+                    break;
+                case "2":
+                    quotes = quotes.OrderByDescending(s => s.CustomerName);
+                    break;
+                case "3":
+                    quotes = quotes.OrderByDescending(s => s.QuoteDate);
+                    break;
+                default:
+                    quotes = quotes.OrderBy(s => s.QuoteDate);
+                    break;
+            }
+
+
+            DeskQuote = await quotes.ToListAsync();
         }
     }
 }
